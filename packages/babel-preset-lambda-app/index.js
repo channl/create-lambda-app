b2a9fbcd552119c410a74f656e4effdbeffec44a
Thomas Mitchell <tmitchel2@googlemail.com>
2017-06-01 22:08:35 +0100

    Fix node target by converting from decimal to string

--- packages/babel-preset-lambda-app/index.js
+++ packages/babel-preset-lambda-app/index.js
@@ -60,7 +60,7 @@ if (env === 'test') {
         require.resolve('babel-preset-env'),
         {
           targets: {
-            node: 6.1,
+            node: '6.1',
           },
           useBuiltIns: false,
           modules: false,
